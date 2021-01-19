4fa59f4319d360eec7a0ed92ead299eb73999bf1
Illidan <illidanrulez@gmail.com>
2021-01-19 17:33:17 +0100

    Added more pointers

--- tool.py
+++ tool.py
@@ -3,7 +3,7 @@ import os
 import click
 from hacktools import common, ws
 
-version = "0.9.4"
+version = "0.9.5"
 data = "ShitoData/"
 romfile = data + "shito.ws"
 rompatch = data + "shito_patched.ws"
